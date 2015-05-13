ab1e8d12f3ff9942b2e5a888e6b0fe1437e63def
Bastian Dupps <dupps@email.de>
2015-05-13 12:45:50 +0200

    authenticate user before any action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :answers
   has_and_belongs_to_many :polls
+
+  validates_uniqueness_of :email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
