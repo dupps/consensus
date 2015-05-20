4bb9204113713f773488da902f327738ec8fbd7a
Bastian Dupps <dupps@email.de>
2015-05-20 16:24:37 +0200

    add user registration test

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -1,5 +1,5 @@
-feature 'AddNewPolls', :type => :feature do
-  it 'should require the user log in before adding a post' do
+feature 'AddNewPoll', :type => :feature do
+  it 'should require the user log in before adding a poll' do
     user = create(:user)
     visit new_poll_path
     within('#new_user') do
@@ -8,5 +8,16 @@ feature 'AddNewPolls', :type => :feature do
     end
     click_link_or_button 'Log in'
     expect(page).to have_content 'Create a new poll'
+  end
+end
+
+feature 'UserRegistration', :type => :feature do
+  it 'allows a user to regiser' do
+    visit new_user_registration_path
+    fill_in 'user_email', :with => 'test@example.com'
+    fill_in 'user_password', :with => 'security'
+    fill_in 'user_password_confirmation', :with => 'security'
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome!'
   end
 end
