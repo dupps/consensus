62c199e925d8fadc0521e2eff8c4362319018674
Bastian Dupps <dupps@email.de>
2015-05-20 10:09:31 +0200

    add more answers controller tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe AnswersController do
         post :create, {choice: {id: @poll.choices.first.id}}
         expect(response.status).to eq 302
       end
+
+      it 'should redirect to the specific poll' do
+        post :create, {choice: {id: @poll.choices.first.id}}
+        expect(response.body).to include(poll_url id: @poll.id)
+      end
+
+      it 'should append url parameter to hide notification' do
+        post :create, {choice: {id: @poll.choices.first.id}}
+        expect(response.body).to include("?hide_already_answered=true")
+      end
     end
   end
 end
